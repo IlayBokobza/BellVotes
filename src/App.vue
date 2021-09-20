7cbb4e11cd702bc5651fdd8163b39e6e4f6406d8
Ilay Bokobza <kanoel200@gmail.com>
2021-09-20 11:18:07 +0300

    added loading screen

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <Navbar/>
     <router-view/>
     <Footer/>
+    <Loading v-if="!$store.state.email"/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import Loading from './components/Loading.vue'
 export default {
   components:{
     Navbar,
-    Footer
+    Footer,
+    Loading
   }
 }
 </script>
