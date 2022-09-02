e446d5040b21ca086f9c0cd0aaca2ab0497a2532
Ilay Bokobza <kanoel200@gmail.com>
2022-09-02 15:51:21 +0300

    fixed cycle

--- server/services/storage.js
+++ server/services/storage.js
@@ -4,22 +4,32 @@ const path = require('path')
 module.exports = class Storage{
     static folder = path.resolve(__dirname,'../data')
 
+    static createDataFolder(){
+        if(!fs.existsSync(this.folder)){
+            fs.mkdirSync(this.folder)
+        }
+    }
+
     static updateSong(base64Data){
         if(!base64Data) return;
-
+        Storage.createDataFolder()
+        
         const data = Buffer.from(base64Data,'base64')
         fs.writeFileSync(`${this.folder}/song.mp3`,data)
     }
 
     static getSong(){
+        Storage.createDataFolder()
         return fs.readFileSync(`${this.folder}/song.mp3`).toString('base64')
     }
 
     static updateDate(){
+        Storage.createDataFolder()
         fs.writeFileSync(`${this.folder}/lastUpdate.txt`,Date.now().toString())
     }
 
     static getDate(){
+        Storage.createDataFolder()
         return fs.readFileSync(`${this.folder}/lastUpdate.txt`).toString()
     }
 
